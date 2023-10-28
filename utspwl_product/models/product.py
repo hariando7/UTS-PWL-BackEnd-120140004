a7011db043b22a24ad0af4914ed612885af6c398
hariando7 <96804614+hariando7@users.noreply.github.com>
2023-10-28 22:39:10 +0700

    Add files via upload

---
+++ utspwl_product/models/product.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, Text
+from .meta import Base
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    price = Column(Integer, nullable=False)
+    gambar = Column(Text, nullable=True)
+    
